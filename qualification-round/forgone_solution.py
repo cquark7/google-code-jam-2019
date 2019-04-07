c507ab70ee03782c512838093d266dafa93836d7
Deepankar Sharma <dash.srmk@gmail.com>
2019-04-07 09:07:51 +0530

    simplified solution for forgone problem

--- qualification-round/forgone_solution.py
+++ qualification-round/forgone_solution.py
@@ -1,19 +1,9 @@
-# Main idea is that 4 can be represented as the sum of two digits:
+# 4 can be represented as the sum of two digits:
 # 4 = 1 + 3 = 2 + 2
 
 for tc in range(int(input())):
-    # take input as a string
     number = input()
     A = number
-    B = ['0']*len(number)
-
-    for idx, digit in enumerate(number):
-        if digit == '4':
-            B[idx] = '1'
-
+    B = ''.join('1' if d == '4' else '0' for d in number).lstrip('0')
     A = A.replace('4', '3')
-
-    # remove leading zeroes
-    B = ''.join(B).lstrip('0')
-
     print('Case #{}: {} {}'.format(tc+1, A, B))
