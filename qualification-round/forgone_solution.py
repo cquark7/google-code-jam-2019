ed61ee85d46bec9714dd02bae737a4dc25627876
Rizwan Hasan <rizwan.hasan486@gmail.com>
2020-04-02 01:32:48 +0600

    Update forgone_solution.py

--- qualification-round/forgone_solution.py
+++ qualification-round/forgone_solution.py
@@ -1,9 +1,9 @@
 # 4 can be represented as the sum of two digits:
 # 4 = 1 + 3 = 2 + 2
 
-for tc in range(int(input())):
-    number = input()
+for _ in range(1, int(input()) + 1):
+    number = input().strip()
     A = number
     B = ''.join('1' if d == '4' else '0' for d in number).lstrip('0')
     A = A.replace('4', '3')
-    print('Case #{}: {} {}'.format(tc+1, A, B))
+    print('Case #{0}: {1} {2}'.format(_, A, B))
