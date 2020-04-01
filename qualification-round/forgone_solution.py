7861d5c44c59284fc27b2ec29f19ed22e80d8a79
Rizwan Hasan <rizwan.hasan486@gmail.com>
2020-04-02 01:45:28 +0600

    simplified the code

--- qualification-round/forgone_solution.py
+++ qualification-round/forgone_solution.py
@@ -1,9 +1,14 @@
 # 4 can be represented as the sum of two digits:
 # 4 = 1 + 3 = 2 + 2
 
-for tc in range(int(input())):
-    number = input()
-    A = number
-    B = ''.join('1' if d == '4' else '0' for d in number).lstrip('0')
+for case in range(1, int(input()) + 1):
+    A = input().strip()
+    B = ''
+    for digit in A:
+        if digit == '4':
+            B += '1'
+        else:
+            B += '0'
+    B = B.lstrip('0')
     A = A.replace('4', '3')
-    print('Case #{}: {} {}'.format(tc+1, A, B))
+    print('Case #{0}: {1} {2}'.format(case, A, B))
